caaff12ccea1b0b0802c122a00257f7739ce69f3
Alvin V <alvin.vassoodaven@colibridigital.io>
2023-11-01 16:33:28 +0000

    Adding more config

--- modules/service_acc/main.tf
+++ modules/service_acc/main.tf
@@ -4,9 +4,11 @@ resource "google_service_account" "service_account" {
   display_name = var.service_account_name
 }
 
+/*
 resource "google_project_iam_member" "iam_member" {
   count   = length(var.roles)
   project = var.project
   role    = var.roles[count.index]
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
+*/
